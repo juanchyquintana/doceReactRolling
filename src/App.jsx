55623aa8899247bde1eb23c67c844c5f8284f666
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-01-29 14:00:20 -0300

    se han creados los archivos que tenía el anterior proyecto para poder agregarle la funcion que falta

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { Container } from "react-bootstrap";
+import FormularioNoticias from "./components/FormularioNoticias";
 
 function App() {
-
   return (
     <>
-      <h1 className="text-center bg-info p-4">Ejercicio Doce</h1>
+      <h1 className="text-center text-uppercase fw-bold bg-warning py-3 border-bottom border-black shadow">
+        Rolling Noticias
+      </h1>
+
+      <Container>
+        <FormularioNoticias />
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
